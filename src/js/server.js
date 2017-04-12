4e5f30e14964e0c8693a9c7746d83cdd3ee1ca7b
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2017-04-12 14:26:07 -0400

    using connect to work with requests for paths.

--- src/js/server.js
+++ src/js/server.js
@@ -1,30 +1,27 @@
+var connect = require('connect');
 var http = require('http');
-var fs = require('fs');
+//var fs = require('fs');
 
 
 
-//404 response
-function send404Response(response) {
-response.writeHead(404, {"Content-type": "text/plain"});
-response.write('Page not found');
-response.end();
-}
 
-//handle user request
-function onRequest(request, response) {
-	if(request.method == 'GET' && (request.url == '/' || request.url == '/index.html')) {
-		response.writeHead(200, {"Content-Type": "text/html"});
-		fs.createReadStream('../html/index.html').pipe(response);
-	} else {
-		send404Response(response);
-	}
-};
+//dicating path
 
+// var app = connect();
 
+// function profile(request, response) {
+// 	console.log("user requested profile");
+// };
 
-http.createServer(onRequest).listen(8888);
-console.log('Server is running....');
+// function form(request, response) {
+// 	console.log("user requested form");
+// };
 
+// app.use('/profile', profile);
+// app.use('/form', form);
+
+// http.createServer(app).listen(8888);
+// console.log('Server is running');
 
 
 
@@ -36,6 +33,31 @@ console.log('Server is running....');
 
 
 
+
+
+
+
+// //404 response
+// function send404Response(response) {
+// response.writeHead(404, {"Content-type": "text/plain"});
+// response.write('Page not found');
+// response.end();
+// }
+
+// //handle user request
+// function onRequest(request, response) {
+// 	if(request.method == 'GET' && (request.url == '/' || request.url == '/index.html')) {
+// 		response.writeHead(200, {"Content-Type": "text/html"});
+// 		fs.createReadStream('../html/index.html').pipe(response);
+// 	} else {
+// 		send404Response(response);
+// 	}
+// };
+
+
+
+// http.createServer(onRequest).listen(8888);
+// console.log('Server is running....');
 
 
 	// console.log('I made a request' + request.url);
