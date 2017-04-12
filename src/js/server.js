bf6911a258dd0c63189ebc3c16adcde2ad46cc92
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2017-04-12 13:43:42 -0400

    building a simple server that response with a
    html file BOO YAH!!

--- src/js/server.js
+++ src/js/server.js
@@ -1,14 +1,44 @@
 var http = require('http');
+var fs = require('fs');
 
 
-function onRequest(request, response) {
-	console.log('I made a request' + request.url);
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.write('Here is your data');
-	response.end();
+
+//404 response
+function send404Response(response) {
+response.writeHead(404, {"Content-type": "text/plain"});
+response.write('Page not found');
+response.end();
 }
+
+//handle user request
+function onRequest(request, response) {
+	if(request.method == 'GET' && (request.url == '/' || request.url == '/index.html')) {
+		response.writeHead(200, {"Content-Type": "text/html"});
+		fs.createReadStream('../html/index.html').pipe(response);
+	} else {
+		send404Response(response);
+	}
+};
 
 
 
 http.createServer(onRequest).listen(8888);
 console.log('Server is running....');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// console.log('I made a request' + request.url);
+	// response.writeHead(200, {'Content-Type': 'text/plain'});
+	// response.write('Here is your data');
+	// response.end();
